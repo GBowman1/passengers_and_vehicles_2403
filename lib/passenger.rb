2db57040ed9474a63158267622d0e64de1e4266e
Garrett Bowman <garrettbowman248@gmail.com>
2024-04-03 10:26:16 -0500

    WIP: can check if passenger is adult

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,5 +3,13 @@ class Passenger
     def initialize(hash)
         @name = hash["name"]
         @age = hash["age"]
+        @is_adult = false
+    end
+
+    def adult?
+        if @age >= 18
+            @is_adult = true
+        end
+        @is_adult
     end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -10,13 +10,25 @@ describe Passenger do
 
     it 'has a name' do
         charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
         expect(charlie.name).to eq('Charlie')
+        expect(taylor.name).to eq('Taylor')
     end
 
     it 'has an age' do
         charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
         expect(charlie.age).to eq(18)
+        expect(taylor.age).to eq(12)
+    end
+
+    it 'can check if passenger is an adult' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        expect(charlie.adult?).to eq(true)
+        expect(taylor.adult?).to eq(false)
     end
 end
