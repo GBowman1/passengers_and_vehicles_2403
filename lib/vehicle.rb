06ad25f9655b558164303db3f6298227897c14fe
Garrett Bowman <garrettbowman248@gmail.com>
2024-04-03 10:41:18 -0500

    WIP: passenger=[], can add passengers

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,13 +2,15 @@ class Vehicle
     attr_reader :year, 
                 :make, 
                 :model,
-                :speeding
+                :speeding,
+                :passengers
 
     def initialize(year, make, model)
         @year = year
         @make = make
         @model = model
         @speeding = false
+        @passengers = []
     end
 
     def speeding?
@@ -17,5 +19,9 @@ class Vehicle
 
     def speed
         @speeding = true
+    end
+
+    def add_passenger(passenger)
+        @passengers << passenger
     end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './lib/vehicle.rb'
+require './lib/passenger.rb'
 
 describe Vehicle do
     it 'it exists' do
@@ -34,5 +35,21 @@ describe Vehicle do
         vehicle.speed
 
         expect(vehicle.speeding?).to eq(true)
+    end
+
+    it 'has passengers' do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+        expect(vehicle.passengers).to eq([])
+
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        jude = Passenger.new({"name" => "Jude", "age" => 20})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        vehicle.add_passenger(charlie)
+        vehicle.add_passenger(jude)
+        vehicle.add_passenger(taylor)
+
+        expect(vehicle.passengers).to eq([charlie, jude, taylor])
     end
 end
