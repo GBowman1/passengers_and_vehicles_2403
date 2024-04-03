df4c49abc4300142082788c548b237c3e7ed2a07
Garrett Bowman <garrettbowman248@gmail.com>
2024-04-03 10:35:08 -0500

    WIP: vehicle has make, model, year

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -3,8 +3,26 @@ require './lib/vehicle.rb'
 
 describe Vehicle do
     it 'it exists' do
-        vehicle = Vehicle.new
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
         
         expect(vehicle).to be_an_instance_of(Vehicle)
+    end
+
+    it 'has a make' do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+        expect(vehicle.make).to eq('Honda')
+    end
+
+    it 'has a model' do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+        expect(vehicle.model).to eq('Civic')
+    end
+
+    it 'has a year' do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+        expect(vehicle.year).to eq('2001')
     end
 end
